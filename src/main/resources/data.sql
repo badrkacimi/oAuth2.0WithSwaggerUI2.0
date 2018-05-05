f78d1632a0fb0ad30f6e26cf94e6c73688c81e96
BADR KACIMI <31140256+BADRKAC@users.noreply.github.com>
2018-05-05 18:45:48 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (user_id, username, password, enabled) VALUES
-	('1', 'badr@hive.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', true),
-	('2', 'ayoub@hive.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', false),
-	('3', 'nidal@hive.com', '$2a$10$D4OLKI6yy68crm.3imC9X.P2xqKHs5TloWUcr6z5XdOqnTrAK84ri', true);
+	('1', 'badr@hive.com', 'e1a378b86bc5c0203239b935e2964ba3', true),
+	('2', 'ayoub@hive.com', '5f4dcc3b5aa765d61d8327deb882cf99', true),
+	('3', 'nidal@hive.com', 'dab3421f64d9dc225b0d9352c76b9e1f', true);
 
